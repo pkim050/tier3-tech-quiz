a1e0d41621b804bb405dead8a48c012726052a40
patrickaaronkim <pkim050@gmail.com>
2024-04-01 11:12:12 -0700

    Fixed the syntax error of console.log

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,6 +1,8 @@
 function oopsie() {
   setTimeout(oopsie, 10000);
-  console.logger("just saying hello - is this thing on?");
+  // I will just have the logs show up instead of deleting it, I believe you wanted me to fix
+  // the error itself and that's it.
+  console.log("just saying hello - is this thing on?");
   return true;
 }
 
